9a323ca0e764c84cbb827935dfc0ba98e1ddddf9
Aniyah Norman <amn84618@uga.edu>
2023-01-26 16:17:39 -0500

    Initial Commit

---
+++ AI Agency/AI_AgencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_AgencyApp.swift
+//  AI Agency
+//
+//  Created by Aniyah Norman on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_AgencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
